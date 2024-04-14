0813a0ae4b27c2ee5cd03032000f2299633533fa
Kebablanca <ibikhalit@hotmail.com>
2024-04-14 23:19:00 +0300

    Commit

---
+++ app/home.jsx
@@ -0,0 +1,13 @@
+import { View, Text, } from 'react-native'
+import React from 'react'
+
+
+
+export default function Home() {
+    return (
+        <View>
+            <Text>This is Home Page</Text>
+        </View>
+    )
+
+}
